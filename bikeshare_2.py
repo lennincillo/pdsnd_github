8ed08d0f8d7f6761e753f92656a2b47b03816daf
Lennincillo <lechamorrom@achs.cl>
2020-10-01 23:54:08 -0300

    docs: Add comments to main function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -192,7 +192,7 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+        #here we call all the fuctions that we has made until now
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
